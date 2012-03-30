82d10a3c7406cacbf33fd34489b80bb405dcf3d9
Hugo Duncan <hugo@hugoduncan.org>
2012-03-29 22:46:57 -0400

    Add missing dependency on tools.logging, and add codox confg

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject pallet-fsm "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [slingshot "0.10.2"]])
+                 [org.clojure/tools.logging "0.2.2"]
+                 [slingshot "0.10.2"]]
+  :profiles {:dev {:dependencies [[codox-md "0.1.0"]]}}
+  :codox {:writer codox-md.writer/write-docs})
