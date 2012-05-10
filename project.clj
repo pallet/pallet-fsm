e717c80439cf761d0e7f3216d4a84c9f13ae9ab7
Hugo Duncan <hugo@hugoduncan.org>
2012-05-10 10:12:10 -0400

    Update to pallet-thread 0.1.0

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/algo.monads "0.1.0"]
                  [org.clojure/tools.logging "0.2.3"]
-                 [pallet-thread "0.1.0-SNAPSHOT"]
+                 [pallet-thread "0.1.0"]
                  [slingshot "0.10.2"]]
   :warn-on-reflection true
   :profiles {:dev {:dependencies [[codox-md "0.1.0"] [codox "0.6.0"]]}}
