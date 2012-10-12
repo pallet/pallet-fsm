97e2955d85104fb29868b2937567710e7752080c
Hugo Duncan <hugo@hugoduncan.org>
2012-10-12 14:05:22 -0400

    Add description, url, license and scm to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,9 @@
 (defproject pallet-fsm "0.1.1-SNAPSHOT"
+  :description "Finite state machine library"
+  :url "https://github.com/pallet/pallet-fsm"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:url "git@github.com:pallet/pallet-fsm.git"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/algo.monads "0.1.0"]
                  [org.clojure/tools.logging "0.2.3"]
